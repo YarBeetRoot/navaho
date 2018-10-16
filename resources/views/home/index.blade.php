3e1734fcf99cba8eb1a8affa848dbe42a7e754ee
YaroslavL <lizogub.yar@gmail.com>
2018-10-16 08:55:09 +0300

    frontend

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main')
+
+@section('content')
+
+    @include('home.slider')
+
+    @include('home.feature')
+
+    @include('home.about')
+
+    @include('home.counter')
+
+    @include('home.service')
+
+    @include('home.pricing-table')
+
+    @include('home.our-team')
+
+    @include('home.testimonial')
+
+    @include('home.clients-brand')
+
+    @include('home.latest-news')
+
+@endsection
